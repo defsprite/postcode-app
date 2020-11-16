647240961242309220774d0ee10530fc1924f044
Christoph Sassenberg <christoph.sassenberg@googlemail.com>
2020-11-16 16:30:55 +0100

    Add system spec for API outage

--- spec/system/postcode_validation_spec.rb
+++ spec/system/postcode_validation_spec.rb
@@ -25,7 +25,21 @@ RSpec.describe "Postcode validation", type: :system do
 
       expect(page).to have_text("Success! The postcode SE1 7QD is available.")
     end
+  end
 
+  context "when postcodes.io is down" do
+    before do
+      Typhoeus.stub(/postcodes\.io/).and_return(Typhoeus::Response.new(code: 0))
+    end
+
+    it "shows an error message" do
+      visit "/postcode_validation/new"
+
+      fill_in "Postcode", with: "SE1 7QD"
+      click_button "Validate Postcode"
+
+      expect(page).to have_text("Oh no! We are currently unable to look up postcodes, please check back later.")
+    end
   end
 
   it "shows an error for an invalid postcode" do
